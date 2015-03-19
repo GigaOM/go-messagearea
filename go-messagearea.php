9bc02ac6339d0ab87d3df2d5d375a8d4a226f396
Matthew Batchelder <borkweb@gmail.com>
2015-03-19 13:21:42 -0400

    Update go-messagearea.php

--- go-messagearea.php
+++ go-messagearea.php
@@ -4,6 +4,8 @@
  * Description: Provides a hook-able message area widget for...messages
  * Author: Gigaom
  * Author URI: http://gigaom.com/
+ * License: GPLv2
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 require_once __DIR__ . '/components/class-go-messagearea.php';
